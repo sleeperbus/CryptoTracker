14c292f63a502417131e199c38e6bc690bebbb21
sleeperbus <sleeperbus@gmail.com>
2025-02-26 21:09:42 +0900

    * 리스트에서 사용할 하나의 아이템을 그린다.

---
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/components/PriceChange.kt
@@ -0,0 +1,87 @@
+package com.plcoding.cryptotracker.crypto.presentation.coin_list.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.plcoding.cryptotracker.crypto.presentation.models.DisplayNumber
+import com.plcoding.cryptotracker.ui.theme.CryptoTrackerTheme
+import com.plcoding.cryptotracker.ui.theme.greenBackground
+
+@Composable
+fun PriceChange(
+    change: DisplayNumber,
+    modifier: Modifier = Modifier
+) {
+    val isDown = change.value < 0.0
+    val contentColor = if (isDown) {
+        MaterialTheme.colorScheme.onErrorContainer
+    } else {
+        Color.Green
+    }
+
+    val backGroundColor = if (isDown) {
+        MaterialTheme.colorScheme.errorContainer
+    } else {
+        greenBackground
+    }
+
+    Row(
+        modifier = Modifier
+            .clip(RoundedCornerShape(size = 100f))
+            .background(backGroundColor)
+            .padding(horizontal = 4.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = if (isDown) {
+                Icons.Default.KeyboardArrowDown
+            } else {
+                Icons.Default.KeyboardArrowUp
+            },
+            contentDescription = null,
+            modifier = Modifier
+                .size(20.dp),
+            tint = contentColor
+        )
+        Text(
+            text = "${change.formatted} %",
+            color = contentColor,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Medium
+        )
+
+    }
+
+}
+
+@Preview
+@PreviewLightDark
+@Composable
+private fun PriceChangePreview() {
+    CryptoTrackerTheme {
+        PriceChange(
+            change = DisplayNumber(
+                value = 2.43,
+                formatted = "2.43"
+            )
+        )
+    }
+}
---
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/models/CoinUi.kt
@@ -0,0 +1,47 @@
+package com.plcoding.cryptotracker.crypto.presentation.models
+
+import androidx.annotation.DrawableRes
+import com.plcoding.cryptotracker.crypto.domain.Coin
+import com.plcoding.cryptotracker.util.getDrawableIdForCoin
+import java.text.NumberFormat
+import java.util.Locale
+
+data class CoinUi(
+    val id: String,
+    val rank: Int,
+    val name: String,
+    val symbol: String,
+    val marketCapUsd: DisplayNumber,
+    val priceUsd: DisplayNumber,
+    val changePercent24Hr: DisplayNumber,
+    @DrawableRes val iconRes: Int
+)
+
+data class DisplayNumber(
+    val value: Double,
+    val formatted: String
+)
+
+fun Coin.toCoinUi(): CoinUi {
+    return CoinUi(
+        id = id,
+        rank = rank,
+        name = name,
+        symbol = symbol,
+        marketCapUsd = marketCapUsd.toDisplayableNumber(),
+        priceUsd = priceUsd.toDisplayableNumber(),
+        changePercent24Hr = changePercent24Hr.toDisplayableNumber(),
+        iconRes = getDrawableIdForCoin(symbol)
+    )
+}
+
+fun Double.toDisplayableNumber(): DisplayNumber {
+    val formatter = NumberFormat.getNumberInstance(Locale.getDefault()).apply {
+        minimumFractionDigits = 2
+        maximumFractionDigits = 2
+    }
+    return DisplayNumber(
+        value = this,
+        formatted = formatter.format(this)
+    )
+}
